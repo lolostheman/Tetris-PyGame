6ada7ae2fb24e584c24acda124c2b4ef44ba903b
Logan Morro <loganmlogan@gmail.com>
2021-08-17 12:18:07 -0400

    Before re-write

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+blocksize = 50
 
 class tetrisBlocks:
     def __init__(self, shape, length, color):
@@ -18,22 +24,80 @@ class tetrisBlocks:
         if self.shape == 'O':
             return [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]
 
-def createMap():
-    running = True
-    while running:
 
-        (width, height) = (300, 540)
-        screen = pygame.display.set_mode((width, height))
-        #pygame.display.flip()
-        blockSize = 20
-        for x in range(0, width, blockSize):
-            for y in range(0, height, blockSize):
-                rect = pygame.Rect(x, y, blockSize, blockSize)
-                pygame.draw.rect(screen, (200, 200, 200), rect, 1)
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+grid = []
+for row in range(13):
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)
+
+
+pygame.init()
+screen = pygame.display.set_mode([500, 800])
+clock = pygame.time.Clock()
+
+
+def drawBlock(shape):
+    running = True
+    if shape == 'T':
+        grid[0][4] = 1
+        grid[0][3] = 1
+        grid[0][5] = 1
+        grid[1][4] = 1
+        grid[2][4] = 1
+    while running:
+        screen.fill(BLACK)
+        for row in range(13):
+
+            for column in range(10):
+                color = WHITE
+                if grid[row][column] == 1:
+                    color = GREEN
+                pygame.draw.rect(screen, color,
+                                 [(5 + blocksize) * column + 5, (5 + blocksize) * row + 5, blocksize, blocksize])
+        shift = 0
+
+        for row in range(13):
+            for column in range(10):
+                if shift < 10:
+                    if grid[row][column] == 1:
+                        grid[row][column] = 0
+                        grid[row+1][column] = 1
+            shift += 1
+
+
+
+
+        clock.tick(1)
+        pygame.display.flip()
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
                 running = False
+
+
+
+
+drawBlock('T')
+pygame.quit()
+
+
+# running = True
+#
+# while running:
+#     screen.fill(BLACK)
+#     for row in range(13):
+#         for column in range(10):
+#             color = WHITE
+#             if grid[row][column] == 1:
+#                 color = GREEN
+#             pygame.draw.rect(screen, color, [(5 + blocksize) * column + 5, (5 + blocksize) * row + 5, blocksize, blocksize])
+#     clock.tick(60)
+#     pygame.display.flip()
+
+
+
+
+
 
 
 
@@ -50,7 +114,7 @@ def createMap():
 #             running = False
 #     pygame.display.update()
 
-createMap()
+
 
 
 
